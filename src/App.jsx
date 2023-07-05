6eb71aa078b83569e04589d7126941984f61a46b
Carmen <path4design@gmail.com>
2023-07-05 13:54:31 +0200

    first commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+          <SkillList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
